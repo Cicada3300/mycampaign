586d2ce976222f695ef2acc0c59156ae6c2a1590
Cicada3300 <36929040+Cicada3300@users.noreply.github.com>
2025-04-11 08:12:07 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+/* script.js */
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for reaching out! Jordan's campaign will be in touch soon.");
+});
